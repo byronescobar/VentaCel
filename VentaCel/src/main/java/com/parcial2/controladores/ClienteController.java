f729a7fa4be111263c484e3f13f5b570eb7e3483
Byron <Byron@BYRON-CASTRO>
2019-12-01 16:39:19 -0300

    ClienteController. Close #5

--- VentaCel/src/main/java/com/parcial2/controladores/ClienteController.java
+++ VentaCel/src/main/java/com/parcial2/controladores/ClienteController.java
@@ -19,42 +19,38 @@ import com.parcial2.repositorios.RepoVenta;
 public class ClienteController {
 
 	@Autowired
-	RepoCliente repoCliente; 
+	RepoCliente repoCliente;
 	@Autowired
 	RepoVenta repoVenta;
-	
-	
-	@GetMapping("/cliente")    //Ruta listado
+
+	@GetMapping("/cliente") // Ruta listado
 	public String listado(Model model) {
 		model.addAttribute("clientes", repoCliente.findAll());
 		return "index";
 	}
-	
-	@GetMapping(value="/registro")  //Ruta nuevo
+
+	@GetMapping(value = "/registro") // Ruta nuevo
 	public String formumario(Model model) {
 		return "registroC";
 	}
-	
-	@PostMapping(value = "/registrar")  //Ruta registrar
+
+	@PostMapping(value = "/registrar") // Ruta registrar
 	public String registrar(@ModelAttribute("cliente") Cliente c, RedirectAttributes flash) {
-			if(c.getId()>0) {
-				flash.addFlashAttribute("exito","Cliente Actualizado con éxito");
-			}else {
-				flash.addFlashAttribute("warning","Guardado Cliente...");
-			}
-			repoCliente.save(c);
+		if (c.getId() > 0) {
+			flash.addFlashAttribute("exito", "Cliente Actualizado con éxito");
+		} else {
+			flash.addFlashAttribute("warning", "Guardado Cliente...");
+		}
+		repoCliente.save(c);
 		return "redirect:/cliente";
 	}
-			
+
 	@GetMapping(value = "/editar/{id}")
-	public String editar(@PathVariable(value="id") int id,Model model) {
-		Cliente c= new Cliente();
-		c=repoCliente.getOne(id);
+	public String editar(@PathVariable(value = "id") int id, Model model) {
+		Cliente c = new Cliente();
+		c = repoCliente.getOne(id);
 		model.addAttribute("cliente", c);
 		return "actualizarC";
 	}
-	
-	
-		
-	
+
 }
